48f92f758efc537ef8724fcfb73a730d57c4063d
jazoo-kat <69760337+jazoo-kat@users.noreply.github.com>
2020-08-16 19:54:20 +0300

    Commit

---
+++ phoneBookWFA - backup/phoneBookWFA/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace phoneBookWFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtPostal = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtLast = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.phonebookBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.phoneBookData = new phoneBookWFA.phoneBookData();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phonenumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.postalCodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phonebookBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneBookData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.label1.Location = new System.Drawing.Point(78, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Etunimi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.label2.Location = new System.Drawing.Point(78, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Osoite";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.label3.Location = new System.Drawing.Point(78, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(216, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Puhelinnumero";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.label4.Location = new System.Drawing.Point(78, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 36);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sukunimi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.label5.Location = new System.Drawing.Point(78, 377);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(230, 36);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Postitoimipaikka";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.label6.Location = new System.Drawing.Point(78, 320);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(182, 36);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Postinumero";
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phonebookBindingSource, "firstName", true));
+            this.txtFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.txtFirst.Location = new System.Drawing.Point(342, 79);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(476, 41);
+            this.txtFirst.TabIndex = 6;
+            // 
+            // txtCity
+            // 
+            this.txtCity.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phonebookBindingSource, "City", true));
+            this.txtCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.txtCity.Location = new System.Drawing.Point(342, 377);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(476, 41);
+            this.txtCity.TabIndex = 7;
+            // 
+            // txtPostal
+            // 
+            this.txtPostal.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phonebookBindingSource, "postalCode", true));
+            this.txtPostal.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.txtPostal.Location = new System.Drawing.Point(342, 320);
+            this.txtPostal.Name = "txtPostal";
+            this.txtPostal.Size = new System.Drawing.Size(476, 41);
+            this.txtPostal.TabIndex = 8;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phonebookBindingSource, "address", true));
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.txtAddress.Location = new System.Drawing.Point(342, 255);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(476, 41);
+            this.txtAddress.TabIndex = 9;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phonebookBindingSource, "phonenumber", true));
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.txtPhone.Location = new System.Drawing.Point(342, 199);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(476, 41);
+            this.txtPhone.TabIndex = 10;
+            // 
+            // txtLast
+            // 
+            this.txtLast.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phonebookBindingSource, "lastName", true));
+            this.txtLast.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.txtLast.Location = new System.Drawing.Point(342, 136);
+            this.txtLast.Name = "txtLast";
+            this.txtLast.Size = new System.Drawing.Size(476, 41);
+            this.txtLast.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.label7.Location = new System.Drawing.Point(884, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 36);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Hae";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F);
+            this.txtSearch.Location = new System.Drawing.Point(1024, 76);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(510, 41);
+            this.txtSearch.TabIndex = 13;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.firstNameDataGridViewTextBoxColumn,
+            this.lastNameDataGridViewTextBoxColumn,
+            this.phonenumberDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn,
+            this.postalCodeDataGridViewTextBoxColumn,
+            this.cityDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.phonebookBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(890, 158);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(644, 330);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.btnNew.Location = new System.Drawing.Point(84, 465);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(94, 38);
+            this.btnNew.TabIndex = 15;
+            this.btnNew.Text = "Uusi";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.btnSave.Location = new System.Drawing.Point(342, 465);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(138, 38);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Tallenna";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.btnDel.Location = new System.Drawing.Point(680, 465);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(138, 38);
+            this.btnDel.TabIndex = 17;
+            this.btnDel.Text = "Poista";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // phonebookBindingSource
+            // 
+            this.phonebookBindingSource.DataMember = "Phonebook";
+            this.phonebookBindingSource.DataSource = this.phoneBookData;
+            // 
+            // phoneBookData
+            // 
+            this.phoneBookData.DataSetName = "phoneBookData";
+            this.phoneBookData.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // firstNameDataGridViewTextBoxColumn
+            // 
+            this.firstNameDataGridViewTextBoxColumn.DataPropertyName = "firstName";
+            this.firstNameDataGridViewTextBoxColumn.HeaderText = "Etunimi";
+            this.firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            // 
+            // lastNameDataGridViewTextBoxColumn
+            // 
+            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "lastName";
+            this.lastNameDataGridViewTextBoxColumn.HeaderText = "Sukunimi";
+            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            // 
+            // phonenumberDataGridViewTextBoxColumn
+            // 
+            this.phonenumberDataGridViewTextBoxColumn.DataPropertyName = "phonenumber";
+            this.phonenumberDataGridViewTextBoxColumn.HeaderText = "Puhelinnumero";
+            this.phonenumberDataGridViewTextBoxColumn.Name = "phonenumberDataGridViewTextBoxColumn";
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Osoite";
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            // 
+            // postalCodeDataGridViewTextBoxColumn
+            // 
+            this.postalCodeDataGridViewTextBoxColumn.DataPropertyName = "postalCode";
+            this.postalCodeDataGridViewTextBoxColumn.HeaderText = "Postinumero";
+            this.postalCodeDataGridViewTextBoxColumn.Name = "postalCodeDataGridViewTextBoxColumn";
+            // 
+            // cityDataGridViewTextBoxColumn
+            // 
+            this.cityDataGridViewTextBoxColumn.DataPropertyName = "City";
+            this.cityDataGridViewTextBoxColumn.HeaderText = "Postitoimipaikka";
+            this.cityDataGridViewTextBoxColumn.Name = "cityDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1711, 712);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtLast);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtPostal);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phonebookBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneBookData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtPostal;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtLast;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.BindingSource phonebookBindingSource;
+        private phoneBookData phoneBookData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phonenumberDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn postalCodeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cityDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ phoneBookWFA - backup/phoneBookWFA/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace phoneBookWFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static phoneBookData db;
+        protected static phoneBookData App
+        {
+            get
+            {
+                if (db == null)
+                    db = new phoneBookData();
+                return db;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string file = string.Format("{0}//data.dat", Application.StartupPath);
+            if (File.Exists(file))
+                App.Phonebook.ReadXml(file);
+            phonebookBindingSource.DataSource = App.Phonebook;
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            //Delete row and save
+            if(MessageBox.Show("Poistetaanko rivi varmasti?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                phonebookBindingSource.RemoveCurrent();
+                App.Phonebook.AcceptChanges();
+                App.Phonebook.WriteXml(string.Format("{0}//data.dat", Application.StartupPath));
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            bool flag = true;
+            int i;
+            //Check phone number. phonenumber can only contain numbers
+            if (txtPhone.Text.Length > 0 && !int.TryParse(txtPhone.Text, out i))
+            {
+                flag = false;
+            }
+            if (flag) {
+                try
+                {
+                    phonebookBindingSource.EndEdit();
+                    App.Phonebook.AcceptChanges();
+                    App.Phonebook.WriteXml(string.Format("{0}//data.dat", Application.StartupPath));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tarkasta puhelinnumero!");
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                App.Phonebook.AddPhonebookRow(App.Phonebook.NewPhonebookRow());
+                phonebookBindingSource.MoveLast();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string txt = txtSearch.Text;
+            if(txt.Length >= 3)
+            {
+                //Get searches. Remove case sensitivicy 
+                var query = from o in App.Phonebook
+                            where o.firstName.ToLower().Contains(txt.ToLower()) || o.lastName.ToLower().Contains(txt.ToLower()) || o.phonenumber.ToLower().Contains(txt.ToLower()) || o.address.ToLower().Contains(txt.ToLower()) || o.postalCode.ToLower().Contains(txt.ToLower()) || o.City.ToLower().Contains(txt.ToLower())
+                            select o;
+                if(query.Count() > 0)
+                {
+                    //IF no data can be foud, show every row
+                    dataGridView1.DataSource = query.ToList();
+                }
+                
+            }
+            else
+            {
+                dataGridView1.DataSource = phonebookBindingSource;
+            }
+        }
+    }
+}
